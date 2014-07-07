45afc0569b1f9f2314ee951933468b815a0c62f8
Lyerech <cheissos3@gmail.com>
2014-07-06 21:14:46 -0300

    Remove Default HUD

--- nightofterror/gamemode/cl_init.lua
+++ nightofterror/gamemode/cl_init.lua
@@ -1,3 +1,10 @@
 //mang
 --gotta
 include( 'shared.lua' )
+
+function hidehud( name )
+	for k, v in pairs({"CHudHealth", "CHudBattery", "CHudAmmo", "CHudSecondaryAmmo", })do
+		if name == v then return false end
+	end
+end
+hook.Add("HUDShouldDraw", "hidehud", hidehud)
